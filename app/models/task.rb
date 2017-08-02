c635c3a31657796ded76804f283bf2d49eee8bf5
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-01 21:53:00 -0400

    Add completed, not_completed, and due_today filter methods to task

--- app/models/task.rb
+++ app/models/task.rb
@@ -11,6 +11,18 @@ class Task < ActiveRecord::Base
     self.all.where('completed = ? AND DATE(complete_by) < ?', false, Date.today)
   end
 
+  def self.completed
+    self.all.where('completed = ?', true)
+  end
+
+  def self.not_completed
+    self.all.where('completed = ?', false)
+  end
+
+  def self.due_today
+    self.all.where('completed = ? AND DATE(complete_by) = ?', false, Date.today)
+  end
+
   def add_project(project)
     projects << project if project && !projects.include?(project)
   end
