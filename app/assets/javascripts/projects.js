541c117c3b25afa978f38414fcce778d524f4912
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-02 12:36:30 -0400

    Project messages display with javascript

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -2,5 +2,9 @@ function showProject(id){
   $.get('/projects/'+id + '.json', function(data){
     $('#project-name').text(data.name);
     $('#project-description').text(data.description);
+    $('#project-messages').html('');
+    data.messages.forEach(function(msg){
+      $('#project-messages').append('<div class="alert alert-'+msg.message_type+'">'+msg.content+'</div>');
+    });
   });
 }
