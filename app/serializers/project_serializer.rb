86540ab0f5b130fa5eea80ce749903924252e397
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-02 14:04:32 -0400

    Add next_id and prev_id to project serializer

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,31 @@
 class ProjectSerializer < ActiveModel::Serializer
   attributes :id, :name, :description
+  attribute :next_id do
+    id = object.id+1
+    max_id = nil;
+    while true
+      break if object.user.projects.find_by(id: id) != nil
+      id+=1
+      max_id = object.user.projects.maximum(:id) unless max_id
+      if id > max_id
+        id=nil
+        break
+      end
+    end
+    id
+  end
+  attribute :prev_id do
+    id = object.id-1
+    while true
+      break if object.user.projects.find_by(id: id) != nil
+      id-=1
+      if id < 1
+        id=nil
+        break
+      end
+    end
+    id
+  end
   has_many :tasks_by_priority, key: 'tasks'
   has_many :messages
 end
