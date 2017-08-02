aee908ade4b3285add61db0a0af6b8e2fabd7736
Patrick Sattelberger <sattelbergerp@gmail.com>
2017-08-02 14:12:12 -0400

    Update js-spec.md, changed projectedserializer to use exists? (optimisation)

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -4,7 +4,7 @@ class ProjectSerializer < ActiveModel::Serializer
     id = object.id+1
     max_id = nil;
     while true
-      break if object.user.projects.find_by(id: id) != nil
+      break if object.user.projects.exists?(id: id)
       id+=1
       max_id = object.user.projects.maximum(:id) unless max_id
       if id > max_id
@@ -17,7 +17,7 @@ class ProjectSerializer < ActiveModel::Serializer
   attribute :prev_id do
     id = object.id-1
     while true
-      break if object.user.projects.find_by(id: id) != nil
+      break if object.user.projects.exists?(id: id)
       id-=1
       if id < 1
         id=nil
